defa636a8eeff4d5ed4f9aaf30b5a7e67cae3535
rizwan <rizwanedu03@gmail.com>
2025-09-13 19:04:09 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
       {/* <Home />
       <Exprecience />
       <GeneralData/>
-      <PortfolioLink/>
-      <Details/> */}
+      <PortfolioLink/> */}
+      <Details/>
     </div>
   );
 };
