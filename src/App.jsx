1fef0544d2b25491475b53f28a93a1dbe7e2a7ab
rizwan <rizwanedu03@gmail.com>
2025-09-13 18:55:06 +0530

    yss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import React from "react";
-import Home from "./Components/Home";
-import Exprecience from "./Components/Exprecience";
-import GeneralData from "./components/GeneralData";
-import PortfolioLink from "./components/PortfolioLink";
-import Details from "./components/Details";
+
 
 const App = () => {
   return (
