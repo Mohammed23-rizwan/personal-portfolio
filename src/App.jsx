e70298026b0437f1ca0572b44117f65d1045cd60
rizwan <rizwanedu03@gmail.com>
2025-09-13 19:30:49 +0530

    added final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import React from "react";
-// import Home from "./Components/Home";
+import Home from "./Components/Home";
 import GeneralData from "./components/GeneralData";
 import PortfolioLink from "./components/PortfolioLink";
 import Details from "./components/Details";
@@ -9,7 +9,7 @@ import ExperienceAll from "./components/ExperienceAll";
 const App = () => {
   return (
     <div className="bg-bgprimary w-full min-h-screen text-txtprimary p-4 sm:p-14 overflow-hidden gap-10 flex flex-col">
-      {/* <Home /> */}
+      <Home />
       <ExperienceAll/>
       <GeneralData/>
       <PortfolioLink/>
