d6bf7bb8091a19d1be61cb98fdc9dbdc1af9d91b
rizwan <rizwanedu03@gmail.com>
2025-09-13 19:05:25 +0530

    added port

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-
+// import React from "react";
+// import Home from "./Components/Home";
+// import Exprecience from "./Components/Exprecience";
+// import GeneralData from "./components/GeneralData";
+import PortfolioLink from "./components/PortfolioLink";
 import Details from "./components/Details";
 
 const App = () => {
@@ -6,8 +10,8 @@ const App = () => {
     <div className="bg-bgprimary w-full min-h-screen text-txtprimary p-4 sm:p-14 overflow-hidden gap-10 flex flex-col">
       {/* <Home />
       <Exprecience />
-      <GeneralData/>
-      <PortfolioLink/> */}
+      <GeneralData/> */}
+      <PortfolioLink/>
       <Details/>
     </div>
   );
