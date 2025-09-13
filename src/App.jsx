ab79ec61eda121de47ff210c101968e2739eb464
rizwan <rizwanedu03@gmail.com>
2025-09-13 19:07:25 +0530

    exp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
 // import Home from "./Components/Home";
-// import Exprecience from "./Components/Exprecience";
+import Exprecience from "./Components/Exprecience";
 import GeneralData from "./components/GeneralData";
 import PortfolioLink from "./components/PortfolioLink";
 import Details from "./components/Details";
@@ -8,8 +8,8 @@ import Details from "./components/Details";
 const App = () => {
   return (
     <div className="bg-bgprimary w-full min-h-screen text-txtprimary p-4 sm:p-14 overflow-hidden gap-10 flex flex-col">
-      {/* <Home />
-      <Exprecience /> */}
+      {/* <Home /> */}
+      <Exprecience />
       <GeneralData/>
       <PortfolioLink/>
       <Details/>
